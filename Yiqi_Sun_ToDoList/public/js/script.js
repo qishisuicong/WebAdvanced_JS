8bf45097cea38f20c6775b1a4c60d78839354b89
qishisuicong <qishisuicong@gmail.com>
2017-03-09 20:57:01 -0500

    update

--- Yiqi_Sun_ToDoList/public/js/script.js
+++ Yiqi_Sun_ToDoList/public/js/script.js
@@ -71,11 +71,24 @@ var init = function(){
     
   });
 };
-// var deleteTask = function(e){
-//   var taskNumber = e.target.parentElement.id;
-//   taskNumber.splice(taskNumber,1);
-//   updateTasks();
 
-// }
+
+
+$('.modal').modal({
+      dismissible: true, // Modal can be dismissed by clicking outside of the modal
+      opacity: .5, // Opacity of modal background
+      inDuration: 300, // Transition in duration
+      outDuration: 200, // Transition out duration
+      startingTop: '4%', // Starting top style attribute
+      endingTop: '10%', // Ending top style attribute
+      ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+        alert("Ready");
+        console.log(modal, trigger);
+      },
+      complete: function() { alert('Closed'); } // Callback for Modal close
+    }
+  );
+
 
 window.onload = init();
+
